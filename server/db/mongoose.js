c6f92efa57886bdcd7819110fa2198b41fc5f1ab
TarneemO <Tarneem.O@gmail.com>
2017-07-05 08:37:54 -0700

    updating local host url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise= global.Promise;
-mongoose.connect('mongodb://Tarneem:1201201200@ds149382.mlab.com:49382/todos' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://Tarneem:1201201200@ds149382.mlab.com:49382/todos' || 'mongodb://localhost:27017/TodoApp/todos');
 
 module.exports = {mongoose};
