0065b9e1df1d03e55243b62793b2ab0013cb7af8
TarneemO <Tarneem.O@gmail.com>
2017-07-05 10:07:35 -0700

    final update url working

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,11 +3,8 @@
 
 var mongoose = require('mongoose');
 //var url = 'mongodb://Tarneem:1201201200@ds149382.mlab.com:49382/todos' || 'mongodb://localhost:27017/TodoApp' 
-db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://Tarneem:1201201200@ds149382.mlab.com:49382/todos'
-}; 
-mongoose.connect(db.mlab || db.localhost);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 mongoose.Promise= global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
 
