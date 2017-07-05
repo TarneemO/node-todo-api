16b793d2e5424c06fff2008b4b437ca091089df9
TarneemO <Tarneem.O@gmail.com>
2017-07-05 10:04:57 -0700

    updating url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@
 
 var mongoose = require('mongoose');
 //var url = 'mongodb://Tarneem:1201201200@ds149382.mlab.com:49382/todos' || 'mongodb://localhost:27017/TodoApp' 
-let db = {
+db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://Tarneem:1201201200@ds149382.mlab.com:49382/todos'
 }; 
