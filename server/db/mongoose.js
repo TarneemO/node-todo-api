bac954fefcf4fbc93e548372ae44c3c31fd02ffe
TarneemO <Tarneem.O@gmail.com>
2017-07-06 10:10:34 -0700

    updating files

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,14 +2,15 @@
 //validation: http://mongoosejs.com/docs/validation.html
 
 var mongoose = require('mongoose');
+mongoose.Promise= global.Promise;
+module.exports = {mongoose};
 //var url = 'mongodb://Tarneem:1201201200@ds149382.mlab.com:49382/todos' || 'mongodb://localhost:27017/TodoApp' 
 db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
  mlab: 'mongodb://Tarneem:1201201200@ds149382.mlab.com:49382/todos'
 }; 
-//mongoose.connect(db.mlab || db.localhost);
-mongoose.connect(db.localhost);
-mongoose.Promise= global.Promise;
+mongoose.connect(db.mlab);
+//mongoose.connect('mongodb://Tarneem:1201201200@ds149382.mlab.com:49382/todos' || 'mongodb://localhost:27017/TodoApp' );
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-module.exports = {mongoose};
+
