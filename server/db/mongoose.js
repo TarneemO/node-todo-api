bf8d99642323a60eccbfc13d501882965bfcb141
TarneemO <Tarneem.O@gmail.com>
2017-07-06 11:59:29 -0700

    Setup separate test and development

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -9,8 +9,8 @@ db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
  mlab: 'mongodb://Tarneem:1201201200@ds149382.mlab.com:49382/todos'
 }; 
-mongoose.connect(db.mlab);
+//mongoose.connect(db.mlab);
 //mongoose.connect('mongodb://Tarneem:1201201200@ds149382.mlab.com:49382/todos' || 'mongodb://localhost:27017/TodoApp' );
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 
